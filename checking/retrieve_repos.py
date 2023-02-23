d789d7915e4143cc677c2a678ddab6bb1f1b1fcd
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2023-02-23 13:54:43 +0100

    Valor por defecto razonable para el arg de práctica

--- checking/retrieve_repos.py
+++ checking/retrieve_repos.py
@@ -199,8 +199,8 @@ def parse_args():
                         help="don't clone repos, assume repos were already cloned")
     parser.add_argument('--students', required=True,
                         help="name of csv file with students, exported from Moodle")
-    parser.add_argument('--practice', default=2,
-                        help="practice number (:all: to retrieve all practices")
+    parser.add_argument('--practice', default=practices.keys()[-1],
+                        help="practice number (:all: to retrieve all practices, default: last practice")
     parser.add_argument('--cloning_dir', default='retrieved',
                         help="directory for cloning retrieved practices")
     parser.add_argument('--testing_dir', default='/tmp/p',
